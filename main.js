1ee9c1959309aa25c53be552dfc229c2c8db3da5
danieldaga <daniel@arhrestauracion.com>
2022-11-16 12:09:38 +0100

    ex1 done

--- main.js
+++ main.js
@@ -1,12 +1,11 @@
 const nameValue = document.querySelector('.inputValue')
 const materialValue = document.querySelector('#materialValue')
-const smallDimensionValue = document.querySelector('#diminuta')
-const mediumDimensionValue = document.querySelector('#mediana')
-const largeDimensionValue = document.querySelector('#grande')
+const sizeBoxValue = document.querySelector('#size')
 const comentsValue = document.querySelector('#coments')
 const sendFormBtn = document.querySelector('#sendForm')
 const returnText = document.querySelector('.returnText')
+
 console.log(nameValue.value);
 sendFormBtn.onclick = () => {
-    returnText.textContent = `${nameValue.value} ha pedido una caja de ${materialValue.value} con unas dimensiones ${comentsValue.value}  `
+    returnText.textContent = `${nameValue.value} ha pedido una caja de ${materialValue.value} con unas dimensiones ${sizeBoxValue.value}s. ${comentsValue.value}  `
 }
