d54cec82146f9fd0a4fa9d74c782cc964aa3c206
danieldaga <daniel@arhrestauracion.com>
2022-11-16 12:31:39 +0100

    done

--- main.js
+++ main.js
@@ -9,3 +9,21 @@ console.log(nameValue.value);
 sendFormBtn.onclick = () => {
     returnText.textContent = `${nameValue.value} ha pedido una caja de ${materialValue.value} con unas dimensiones ${sizeBoxValue.value}s. ${comentsValue.value}  `
 }
+
+
+const evaluateBtn = document.querySelector('.evaluate-btn')
+const inputText = document.querySelector('.input-text')
+const resultGame = document.querySelector('.result')
+
+evaluateBtn.onclick = () => {
+    if (inputText.value === '1') {
+        resultGame.textContent = 'El jugador ha perdido'
+    } else if (inputText.value === '2') {
+        resultGame.textContent = 'El jugador ha perdido'
+    } else if (inputText.value === '3') {
+        resultGame.textContent = 'El jugador ha Ganado'
+    }else{
+        resultGame.textContent = 'Vuelve a probar suerte'
+
+    }
+}
