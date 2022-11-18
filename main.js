f2c8758ab87888d828924bd22ae4921ae8ecca38
danieldaga <daniel@arhrestauracion.com>
2022-11-18 17:46:25 +0100

    done

--- main.js
+++ main.js
@@ -1,14 +1,28 @@
 const nameValue = document.querySelector('.inputValue')
 const materialValue = document.querySelector('#materialValue')
-const sizeBoxValue = document.querySelector('#size')
+const sizeBoxValue = document.querySelectorAll('#size')
 const comentsValue = document.querySelector('#coments')
 const sendFormBtn = document.querySelector('#sendForm')
 const returnText = document.querySelector('.returnText')
 
-console.log(nameValue.value);
+
 sendFormBtn.onclick = () => {
-    returnText.textContent = `${nameValue.value} ha pedido una caja de ${materialValue.value} con unas dimensiones ${sizeBoxValue.value}s. ${comentsValue.value}  `
+    
+    let sizeBoxValueResult = ''
+    sizeBoxValue.forEach(element => {
+        if (element.checked) {
+            sizeBoxValueResult += element.value
+        }
+    });
+    returnText.textContent = `${nameValue.value} ha pedido una caja de ${materialValue.value} con unas dimensiones ${sizeBoxValueResult}s. ${comentsValue.value}  `
 }
+
+
+
+
+
+
+////////////////////
 
 
 const evaluateBtn = document.querySelector('.evaluate-btn')
